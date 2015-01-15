af71e307d1e3587c58c462713b7d29fb83f2be12
taylormonacelli <tailor@u.washington.edu>
2015-01-15 12:54:22 -0800

    Create localhost:1124 too for local forwarding over ssh

--- rdp_test.py
+++ rdp_test.py
@@ -19,3 +19,26 @@ for ip in ipcalc.Network('10.0.3.0/24'):
     name = "%s%s" % (name_prefix,octet)
     r = RDP.RDP(name,ip,'Administrator','Stre@mb0x')
     print r.display()
+
+
+# Create connection lo125 to connect to localhost:1125 for example
+
+name_prefix="lo"
+
+for ip in ipcalc.Network('10.0.2.0/24'):
+    octet = re.search( r'(\d+)$', str(ip)).group(1)
+    name = "%s%s" % (name_prefix,octet)
+    om = int(str(octet)) + 1000
+    ip = 'localhost:%s' % om
+    r = RDP.RDP(name,ip,'Administrator','Stre@mb0x')
+    print r.display()
+
+name_prefix="ln"
+
+for ip in ipcalc.Network('10.0.3.0/24'):
+    octet = re.search( r'(\d+)$', str(ip)).group(1)
+    name = "%s%s" % (name_prefix,octet)
+    om = int(str(octet)) + 1000
+    ip = 'localhost:%s' % om
+    r = RDP.RDP(name,ip,'Administrator','Stre@mb0x')
+    print r.display()
